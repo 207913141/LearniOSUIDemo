8a9e795bb7ff5f486b11f4015cea4271c29fc022
william fee <williamfee@williamdeMac.local>
2018-11-09 15:10:06 +0800

    Initial Commit

---
+++ LearniOSUIDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LearniOSUIDemo
+//
+//  Created by william fee on 2018/11/9.
+//  Copyright © 2018年 william fee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LearniOSUIDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LearniOSUIDemo
+//
+//  Created by william fee on 2018/11/9.
+//  Copyright © 2018年 william fee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
